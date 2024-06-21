087b7b8c1a45235f7bb57f8b1c1a6607d8a7e21d
Jagtap-Pranjal <162295646+Jagtap-Pranjal@users.noreply.github.com>
2024-06-21 12:26:53 +0530

    Add files via upload

---
+++ Practice_Programs/string_len.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("The length of the string is: " + input.Length);
+    }
+}
