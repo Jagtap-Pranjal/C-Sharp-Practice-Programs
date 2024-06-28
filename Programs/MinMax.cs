68c9a4b2228b6f4176cdd29dabd3cfc1a23c4b1c
Jagtap-Pranjal <162295646+Jagtap-Pranjal@users.noreply.github.com>
2024-06-28 14:54:44 +0530

    Add files via upload

---
+++ Programs/MinMax.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Max_min
+{
+    class Mm{
+
+        static int a=10, b=20;
+        static void Main(string [] args){
+            if(a>b){
+                Console.WriteLine("A is Greater than B");
+            }
+            if(b>a)
+            {
+                Console.WriteLine("B is Greater than A");
+            }
+            else
+            {
+                Console.WriteLine("A is equal to B");
+            }
+        }
+    }
+}
