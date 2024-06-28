f57f051085309490ea6d166602e8d13aa05df10d
Jagtap-Pranjal <162295646+Jagtap-Pranjal@users.noreply.github.com>
2024-06-28 15:11:08 +0530

    Add files via upload

---
+++ Programs/logical.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Logical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 15, y = 10;
+            bool a = true, result;
+            // AND operator
+            result = (x <= y) && (x > 10);
+            Console.WriteLine("AND Operator: " + result);
+            // OR operator
+            result = (x >= y) || (x < 5);
+            Console.WriteLine("OR Operator: " + result);
+            //NOT operator
+            result = !a;
+            Console.WriteLine("NOT Operator: " + result);
+        }
+    }
+}
